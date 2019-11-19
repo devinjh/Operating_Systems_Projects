bffeb4333a43263ab4ec5251d99540fea716ee7b
Devin <djh155@zips.uakron.edu>
2019-11-19 16:28:45 -0500

    Help function works. But very messy

--- shell.c
+++ shell.c
@@ -57,6 +57,7 @@ struct command_t {
 int parseCommand(char *, struct command_t *);
 void printPrompt();
 void readCommand(char *);
+void printHelp();
 
 int main(int argc, char *argv[])
 {
@@ -108,8 +109,9 @@ int main(int argc, char *argv[])
         realCommand = "echo";
       }
       else if(strcmp(command.name, "H\0") == 0) {
-        char *helpMe = "you called help";
-        printf("%s\n", helpMe);
+        /*char *helpMe = "you called help";
+        printf("%s\n", helpMe);*/
+	printHelp();
       }
       else if(strcmp(command.name, "L\0") == 0) {
         printf("\n");
@@ -190,3 +192,12 @@ void readCommand(char *buffer)
 }
 
 /* End printPrompt and readCommand */
+
+/* Print the help function's information. */
+
+void printHelp()
+{
+  printf("%s ", "C file1 file2 Copy; create file2, copy all bytes of file1 to file2 without deleting file1. D file Delete the named file. E comment Echo; display comment on screen followed by a new line (multiple spaces/tabs may be reduced to a single space); if no argument simply issue a new prompt. H Help; display the user manual, described below. L List the contents of the current directory; see below. M file Make; create the named text file by launching a text editor. P file Print; display the contents of the named file on screen. Q Quit the shell. S Surf the web by launching a browser as a background process. W Wipe; clear the screen. X program Execute the named program.");
+}
+
+/* End help function's information. */
